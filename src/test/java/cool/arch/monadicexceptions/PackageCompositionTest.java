a47bf2f75bb2dccb3fdb829e9e8e739074376591
Matthew Werny <matthewwerny+github@gmail.com>
2015-08-23 23:57:36 -0600

    Fixed headers

--- src/test/java/cool/arch/monadicexceptions/PackageCompositionTest.java
+++ src/test/java/cool/arch/monadicexceptions/PackageCompositionTest.java
@@ -1,23 +1,36 @@
 package cool.arch.monadicexceptions;
 
+/*
+ * @formatter:off
+ * cool.arch.monadicexceptions:monadic-exceptions
+ * %%
+ * Copyright (C) 2015 CoolArch
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * @formatter:on
+ */
+
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import java.util.HashSet;
-
-/*
- * #%L cool.arch.monadicexceptions:monadic-exceptions %% Copyright (C) 2015 CoolArch %%
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions
- * and limitations under the License. #L%
- */
 
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -41,23 +54,16 @@ public class PackageCompositionTest {
 		//		assertEquals("java.util.function", FUNCTIONAL_INTERFACE_PACKAGE);
 		assertEquals("cool.arch.monadicexceptions", THROWABLE_FUNCTIONAL_INTERFACE_PACKAGE);
 
-		final Set<String> expectedFunctionalInterfaceNames =
-			new HashSet<>(asList("BiConsumer", "BiFunction", "BinaryOperator", "BiPredicate", "BooleanSupplier",
-				"Consumer", "DoubleBinaryOperator", "DoubleConsumer", "DoubleFunction", "DoublePredicate",
-				"DoubleSupplier", "DoubleToIntFunction", "DoubleToLongFunction", "DoubleUnaryOperator", "Function",
-				"IntBinaryOperator", "IntConsumer", "IntFunction", "IntPredicate", "IntSupplier",
-				"IntToDoubleFunction", "IntToLongFunction", "IntUnaryOperator", "LongBinaryOperator", "LongConsumer",
-				"LongFunction", "LongPredicate", "LongSupplier", "LongToDoubleFunction", "LongToIntFunction",
-				"LongUnaryOperator", "ObjDoubleConsumer", "ObjIntConsumer", "ObjLongConsumer", "Predicate", "Supplier",
-				"ToDoubleBiFunction", "ToDoubleFunction", "ToIntBiFunction", "ToIntFunction", "ToLongBiFunction",
-				"ToLongFunction", "UnaryOperator"));
+		final Set<String> expectedFunctionalInterfaceNames = new HashSet<>(asList("BiConsumer", "BiFunction", "BinaryOperator", "BiPredicate", "BooleanSupplier", "Consumer", "DoubleBinaryOperator",
+			"DoubleConsumer", "DoubleFunction", "DoublePredicate", "DoubleSupplier", "DoubleToIntFunction", "DoubleToLongFunction", "DoubleUnaryOperator", "Function", "IntBinaryOperator",
+			"IntConsumer", "IntFunction", "IntPredicate", "IntSupplier", "IntToDoubleFunction", "IntToLongFunction", "IntUnaryOperator", "LongBinaryOperator", "LongConsumer", "LongFunction",
+			"LongPredicate", "LongSupplier", "LongToDoubleFunction", "LongToIntFunction", "LongUnaryOperator", "ObjDoubleConsumer", "ObjIntConsumer", "ObjLongConsumer", "Predicate", "Supplier",
+			"ToDoubleBiFunction", "ToDoubleFunction", "ToIntBiFunction", "ToIntFunction", "ToLongBiFunction", "ToLongFunction", "UnaryOperator"));
 
 		//		expectedFunctionalInterfaceNames.forEach(System.out::println);
 
-		final Set<String> ourInterfacesAndTestNames =
-			new Reflections(new ConfigurationBuilder().setUrls(
-				ClasspathHelper.forPackage(THROWABLE_FUNCTIONAL_INTERFACE_PACKAGE))
-				.setScanners(new ResourcesScanner())).getResources(Pattern.compile(".*\\.class"))
+		final Set<String> ourInterfacesAndTestNames = new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forPackage(THROWABLE_FUNCTIONAL_INTERFACE_PACKAGE))
+			.setScanners(new ResourcesScanner())).getResources(Pattern.compile(".*\\.class"))
 				.stream()
 				.filter(s -> s.startsWith("cool/arch/monadicexceptions/Throwable"))
 				.map(s -> s.replaceAll("^cool\\/arch\\/monadicexceptions\\/", ""))
